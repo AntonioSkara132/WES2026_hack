ce406c8e57523ff5e5b239042da226d167aa9697
AntonioSkara132 <conio1240@gmail.com>
2026-03-28 18:06:30 +0100

    messages via tcp

--- main/app_main.c
+++ main/app_main.c
@@ -1,14 +1,12 @@
-/**
-* @file main.c
 
-* @brief 
-* 
-* COPYRIGHT NOTICE: (c) 2022 Byte Lab Grupa d.o.o.
-* All rights reserved.
-*/
+
 
 //--------------------------------- INCLUDES ----------------------------------
 #include "gui.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "esp_event.h"
 //---------------------------------- MACROS -----------------------------------
 
 //-------------------------------- DATA TYPES ---------------------------------
@@ -20,9 +18,30 @@
 //------------------------------- GLOBAL DATA ---------------------------------
 
 //------------------------------ PUBLIC FUNCTIONS -----------------------------
+
+#define TASK_STACK_DEPTH 2048
+extern void tcp_client(void);
+void tcp_task(void *pVparameters){
+   tcp_client();
+}
+
+
 void app_main(void)
 {
    gui_init();
+   ESP_ERROR_CHECK(nvs_flash_init());
+   ESP_ERROR_CHECK(esp_netif_init());
+   ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+   /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+   * Read "Establishing Wi-Fi or Ethernet Connection" section in
+   * examples/protocols/README.md for more information about this function.
+   */
+   ESP_ERROR_CHECK(example_connect());
+
+   xTaskCreate(tcp_task, "tcp_client", TASK_STACK_DEPTH, NULL, 0, NULL);
+   tcp_client();
+
 }
 
 //---------------------------- PRIVATE FUNCTIONS ------------------------------
